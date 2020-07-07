5af43e7cba2faa68d527360407ad4decb1b60c0f
FR3NKru <66692189+FR3NKru@users.noreply.github.com>
2020-07-07 23:25:33 +0300

    Revert "hw 6"

--- homework/hw6/task2.py
+++
@@ -1,7 +0,0 @@
-class Road:
-    def __init__(self, length, width):
-        self.__length = length
-        self.__width = width
-
-    def ashalt_mass(self):
-        return self.__width * self.__length * 25 * 1
